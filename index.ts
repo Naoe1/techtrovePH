4a139db723373438532127a0378a2d78c85f00e7
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-26 14:07:58 +0800

    Add error handling

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, {ErrorRequestHandler} from "express";
 import bodyParser from "body-parser";
+import productsRoutes from "./routes/product";
 
 const app = express();
 
@@ -12,11 +13,23 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/products', productsRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hellowe world!")
 });
 
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    console.error(err);
+    res.status(err.statusCode).json({
+      error: err.message || 'Internal Server Error',
+    });
+  };
+  
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
