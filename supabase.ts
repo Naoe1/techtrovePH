5c545b2d6418125143975e525d497ec9a6bdbc6b
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-26 13:28:26 +0800

    Add supabase connection

---
+++ supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+import 'dotenv/config'
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseKey = process.env.SUPABASE_KEY as string
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export {supabase}
