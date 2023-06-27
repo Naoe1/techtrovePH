fb3ac10d8bc65eb0fe7828cc12c646cae1094e7f
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-27 10:28:59 +0800

    update types

--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+import { Database } from './utils/schema'
 import 'dotenv/config'
 const supabaseUrl = process.env.SUPABASE_URL as string
 const supabaseKey = process.env.SUPABASE_KEY as string
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export {supabase}
