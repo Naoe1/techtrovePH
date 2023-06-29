5b81542ec3bf95f6857cc7e588cff2345d161da9
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-29 16:50:45 +0800

    allow receiving of props

--- client/src/components/TableBody.tsx
+++ client/src/components/TableBody.tsx
@@ -1,3 +1,8 @@
+interface Product {
+    full_name: string,
+    uid: string,
+    min_price: number
+}
 interface TableColumn {
     label: string;
     property: string;
@@ -6,9 +11,15 @@ interface TableColumn {
 interface TableBodyProps<T> {
     data: T[];
     columns: TableColumn[];
+    isLoading?: boolean;
 }
 
-const TableBody = <T extends { productLink: string, productName: string }>({ data, columns }: TableBodyProps<T>) => {
+const TableBody = <T extends Product>({ data, columns, isLoading }: TableBodyProps<T>) => {
+    if (isLoading) {
+        return <tbody className="bg-slate-900">
+            <tr className="sm:px-2"><td colSpan={6}>Loading ...</td></tr>
+        </tbody>
+    }
     return (
         <>
             <tbody className="bg-slate-900">
@@ -16,20 +27,29 @@ const TableBody = <T extends { productLink: string, productName: string }>({ dat
                     data.map((product, index) => (
                         <tr className="sm:px-2" key={index}>
                             {columns.map((header, index) => {
-                                if (index === 0) {
+                                if (header.property === 'full_name') {
                                     return (
                                         <td key={index}><div className="flex justify-center">
-                                            <div className="text-slate-200 font-bold flex flex-col ps-4 text-sm xl:text-base justify-center sm:justify-center">
+                                            <div className="text-slate-200 font-medium flex flex-col ps-4 text-sm xl:text-base justify-center sm:justify-center">
                                                 <div>
-                                                    {product.productName}
+                                                    {product.full_name}
                                                 </div>
                                             </div>
                                         </div></td>
                                     )
                                 }
+                                if (header.property === 'min_price') {
+                                    return (
+                                        <td key={index}>
+                                            <div className="text-xs text-center text-slate-200">
+                                                {product['min_price'].toFixed(2)}
+                                            </div>
+                                        </td>
+                                    )
+                                }
                                 return (
                                     <td key={index}>
-                                        <div className="text-xs text-center text-slate-50">
+                                        <div className="text-xs text-center text-slate-200">
                                             {product[header.property as keyof typeof product]}
                                         </div>
                                     </td>
