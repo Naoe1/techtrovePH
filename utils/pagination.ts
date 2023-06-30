6cef6d3fe932bdbe1760833d39a01d73c9cfe9d6
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-30 09:43:57 +0800

    update pagination

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,7 +1,6 @@
-export const getPagination = (page?: number, limit = 20) => {
-    const from = page ? page * limit : 0;
-    const to = page ? from + limit - 1 : limit - 1
-
+export const getPagination = (page? : number, limit = 50) => {
+    const from = page ? (page - 1) * limit + 1 : 1;
+    const to = page ? from + limit - 1 : limit;
 
     return { from, to };
 };
