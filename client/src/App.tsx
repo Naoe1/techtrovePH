058bf42a18ba42e3e6149b43324dd01276039f37
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-04 19:04:33 +0800

    Basic app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Products from "./views/Products"
-
+import Home from "./views/Home";
+import Product from './views/Product';
+import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
-      <Products />    
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/">
+            <Route path=":category" element={<Products />} />
+            <Route path=":category/:productId" element={<Product />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
