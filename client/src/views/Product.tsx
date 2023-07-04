de837e26e8d53ba1807eba8d6897df1e9e84f58e
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-04 19:05:54 +0800

    Product views

---
+++ client/src/views/Product.tsx
@@ -0,0 +1,13 @@
+import Aside from "../components/Aside"
+import Product from "../components/ProductInfo"
+
+const Products = () => {
+    return (
+        <div className="flex">
+            <Aside />
+            <Product/>
+        </div>
+    )
+}
+
+export default Products
