161ec195d82ade2759b6cec0ce9d91790165e531
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-14 17:10:17 +0800

    add pagination logic

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -1,18 +1,19 @@
 import { Dispatch, SetStateAction } from 'react'
-const Pagination = ({ page, setPage }: { page: number, setPage: Dispatch<SetStateAction<number>> }) => {
-    const from =  page ? (page - 1) * 50 + 1 : 1;
-    const to = page ? from + 50 - 1 : 50;
+
+const Pagination = ({ page, setPage, count }: { page: number, setPage: Dispatch<SetStateAction<number>>, count: number }) => {
+    const from = page ? (page - 1) * 50 + 1 : 1;
+    const to = Math.min(page ? from + 50 - 1 : 50, count as number);
     return (
-        <div className="flex flex-col items-center bg-slate-900 p-3 py-6">
-            <span className="text-sm text-gray-700 dark:text-gray-400" onClick={() => console.log(page)}>
-                Showing <span className="font-semibold text-gray-900 dark:text-white">{ from }</span> to <span className="font-semibold text-gray-900 dark:text-white">{to}</span> of <span className="font-semibold text-gray-900 dark:text-white">100</span> Entries
+        count && <div className="flex flex-col items-center bg-slate-900 p-3 py-6">
+            <span className="text-sm text-gray-700 dark:text-gray-400">
+                Showing <span className="font-semibold text-gray-900 dark:text-white">{from}</span> to <span className="font-semibold text-gray-900 dark:text-white">{to}</span> of <span className="font-semibold text-gray-900 dark:text-white">{count}</span> Entries
             </span>
             <div className="inline-flex mt-2 xs:mt-0">
                 <button disabled={page <= 1} onClick={() => setPage(prev => prev - 1)} className="disabled:bg-slate-950 disabled:pointer-events-none
- px-4 py-2 text-sm font-medium rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+ px-4 py-2 text-sm font-medium rounded-l  bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white">
                     Prev
                 </button>
-                <button onClick={() => setPage(prev => prev + 1)} className="px-4 py-2 text-sm font-medium border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                <button disabled={(from + 50) > count} onClick={() => setPage(prev => prev + 1)} className="disabled:bg-slate-950 disabled:pointer-events-none px-4 py-2 text-sm font-medium border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                     Next
                 </button>
             </div>
