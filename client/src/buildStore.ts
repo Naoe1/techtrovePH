e3d85fca2fd79ea4de98a32f08dc730bdfdcc04a
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-16 18:32:07 +0800

    updated store with other components

--- client/src/buildStore.ts
+++ client/src/buildStore.ts
@@ -12,16 +12,27 @@ interface PCBuilderState {
     videoCard: Description | null;
     powerSupply: Description | null;
     storage: Description[] | [];
+    memory: Description[] | [];
+    cooler: Description | null;
+    chassis: Description | null;
+
     setProcessor: (processor: Description) => void;
     setMotherboard: (motherboard: Description) => void;
     setVideoCard: (videoCard: Description) => void;
-    setStorage: (storage: Description) => void;
     setPowerSupply: (powerSupply: Description) => void;
+    setStorage: (storage: Description) => void;
+    setMemory: (memory: Description) => void;
+    setCooler: (cooler: Description) => void;
+    setChassis: (chassis: Description) => void;
+
     removeProcessor: () => void;
     removeMotherboard: () => void;
     removeVideoCard: () => void;
     removePowerSupply: () => void;
-    removeStorage: (uid: string) => void;
+    removeStorage: (index: number) => void;
+    removeMemory: (index: number) => void;
+    removeCooler: () => void;
+    removeChassis: () => void;
 }
 
 
@@ -31,6 +42,9 @@ const usePCBuilderStore = create<PCBuilderState>((set) => ({
     videoCard: null,
     powerSupply: null,
     storage: [],
+    memory: [],
+    cooler: null,
+    chassis: null,
     setProcessor: (processor: Description) => set(() => ({ processor: processor })),
     setMotherboard: (motherboard: Description) => set(() => ({ motherboard: motherboard })),
     setVideoCard: (videoCard: Description) => set(() => ({ videoCard: videoCard })),
@@ -38,13 +52,24 @@ const usePCBuilderStore = create<PCBuilderState>((set) => ({
     setStorage: (storage: Description) => set((state) => ({
         storage: [...state.storage, storage],
     })),
+    setMemory: (memory: Description) => set((state) => ({
+        memory: [...state.memory, memory],
+    })),
+    setCooler: (cooler: Description) => set(() => ({ cooler: cooler })),
+    setChassis: (chassis: Description) => set(() => ({ chassis: chassis })),
+
     removeProcessor: () => set(() => ({ processor: null })),
     removeMotherboard: () => set(() => ({ motherboard: null })),
     removeVideoCard: () => set(() => ({ videoCard: null })),
     removePowerSupply: () => set(() => ({ powerSupply: null })),
-    removeStorage: (uid: string) => set((state) => ({
-        storage: state.storage.filter((item) => item.uid !== uid),
+    removeStorage: (index: number) => set((state) => ({
+        storage: state.storage.filter((_, i) => i !== index),
     })),
+    removeMemory:(index: number) => set((state) => ({
+        memory: state.memory.filter((_, i) => i !== index),
+      })),
+    removeCooler: () => set(() => ({ cooler: null })),
+    removeChassis: () => set(() => ({ chassis: null })),
 }));
 
 export default usePCBuilderStore;
