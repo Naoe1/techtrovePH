a1e69452f23cb30f797b2acabe3e70f043f50d9e
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-16 18:29:59 +0800

    add route for /list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Products from "./views/Products"
 import Home from "./views/Home";
 import Product from './views/Product';
 import Navbar from './components/Navbar';
+import List from './views/List';
 const App = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App = () => {
             <Route path=":category" element={<Products />} />
             <Route path=":category/:productId" element={<Product />} />
           </Route>
+          <Route path="/list" element={<List/>} />
         </Routes>
       </Router>
     </>
