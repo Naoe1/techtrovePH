5da18278d46e8c6036e8ec1abc33b0b615e5dabd
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-19 15:34:20 +0800

    update schema

--- utils/categoryColumn.ts
+++ utils/categoryColumn.ts
@@ -1,7 +1,12 @@
 const categoryColumns = {
   video_cards: 'full_name,brand,name,uid,model,gpu_spec_ref,gpu_spec(*),boost_clock_mhz,min_price',
-  processors: 'full_name,socket,uid,cores,threads,base_clock_ghz,boost_clock_ghz,integrated_gpu,min_price',
+  processors: 'full_name,socket,uid,cores,threads,integrated_gpu,min_price,tdp_w',
   motherboards: 'full_name,socket,formfactor,memory_capacity_gb,ram_slots,uid,min_price',
+  power_supply: 'full_name,link,min_price,vendor_id',
+  memory: 'full_name,link,min_price,vendor_id',
+  storage: 'full_name,link,min_price,vendor_id',
+  chassis: 'full_name,link,min_price,vendor_id',
+  cpu_cooler: 'full_name,link,min_price,vendor_id',
 };
 
 export default categoryColumns
