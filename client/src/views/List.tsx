1deea9432e99a46579f6d2813768ce184e621c15
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-24 18:09:40 +0800

    add listlink component to list view

--- client/src/views/List.tsx
+++ client/src/views/List.tsx
@@ -11,6 +11,8 @@ import { ReactComponent as Storage } from '../assets/storage.svg'
 import { ReactComponent as Cooler } from '../assets/cooler.svg'
 import Item from "../components/BuildItem"
 import { Link } from "react-router-dom"
+import ListLink from "../components/ListLink"
+
 const List = () => {
     const { processor, motherboard, powerSupply, storage, videoCard, chassis, memory, cooler,
         removeVideoCard, removeProcessor, removeMotherboard, removePowerSupply,
@@ -81,13 +83,14 @@ const List = () => {
 
     ];
     return (
-        <div className="text-[#b4bcd0] pt-28">
+        <div className="text-[#b4bcd0] py-28">
+            <ListLink/>
             <div className="flex flex-col min-h-[500px] p-6 rounded-md border border-[#1f2571eb] max-w-2xl mx-auto list-bg">
                 {items.map(({ item, onRemove, itemName, icon, linkTo }) => (
                     <Fragment key={itemName}>
                         <div className="flex items-center">
                             <div className="w-8 mr-2 sm:mr-4">{icon}</div>
-                            <div className="text-slate-200 font-bold flex items-center" onClick={() => console.log(memory)}>
+                            <div className="text-slate-200 font-bold flex items-center">
                                 {itemName}
                             </div>
                             <div className="border-t-2 grow w-1 ml-3 border-gray-600"></div>
