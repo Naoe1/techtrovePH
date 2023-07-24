709c96384f9fb39e253fecce61b1ed67a9926776
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-24 21:20:04 +0800

    fix drawer on small screen

--- client/src/components/ProductInfo.tsx
+++ client/src/components/ProductInfo.tsx
@@ -60,13 +60,14 @@ const Product = () => {
             <div className="flex flex-col bg-slate-900 text-slate-50 min-h-[820px] w-full">
                 <div className="flex h-14 border-b border-slate-700"></div>
                 <div className='flex flex-col sm:mx-16 my-8 mx-4'>
-                    <div className="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800" role="alert">
+                    <DrawerComponent />
+                    <div className="mt-2 flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800" role="alert">
                         <svg className="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                         </svg>
                         <span className="sr-only">Info</span>
-                        <div>
-                            <span className="font-medium">Warning alert!</span> The product information provided may not be categorized correctly and could have inaccurate specs; please cross-check the details on the official page for accuracy and legitimacy.
+                        <div className='text-sm'>
+                            <span className="font-medium">Warning alert!</span> The provided product information may be categorized incorrectly and contain inaccurate specifications. Please cross-check the details on the official page for accuracy.
                         </div>
                     </div>
                     <h1 className='text-xl'>{data.full_name}</h1>
--- client/src/views/Product.tsx
+++ client/src/views/Product.tsx
@@ -1,12 +1,10 @@
 import Aside from "../components/Aside"
-import DrawerComponent from "../components/Drawer"
 import Product from "../components/ProductInfo"
 
 const Products = () => {
     return (
         <div className="flex">
             <Aside />
-            <DrawerComponent/>
             <Product/>
         </div>
     )
