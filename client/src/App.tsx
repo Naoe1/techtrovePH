f21764d5ebeabc2c19121d044b227aa244c67e95
Eoan <gabriel.naoe@adamson.edu.ph>
2023-07-26 20:40:40 +0800

    add 404 route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Product from './views/Product';
 import Navbar from './components/Navbar';
 import List from './views/List';
 import Build from './views/Build';
+import NotFound from './components/NotFound';
 const App = () => {
   return (
     <>
@@ -12,11 +13,11 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products/" />
           <Route path="/products/:category" element={<Products />} />
           <Route path="/products/:category/:productId" element={<Product />} />
           <Route path="/list" element={<List />} />
           <Route path="list/:listId" element={<Build />} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
       </Router>
     </>
